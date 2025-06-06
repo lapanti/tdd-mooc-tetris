1906804a28a227418f741f5dd6e86a4e2a14b8cf
Lauri Lavanti <laurilavanti@gmail.com>
2025-06-06 09:41:10 +0300

    feat: drop

--- src/Board.ts
+++ src/Board.ts
@@ -1,14 +1,23 @@
 export class Board {
   width: number;
   height: number;
+  board: string;
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
+    this.board = `...
+...
+...
+`;
   }
 
   toString() {
-    return `...
+    return this.board;
+  }
+
+  drop(block: string) {
+    this.board = `.${block}.
 ...
 ...
 `;
