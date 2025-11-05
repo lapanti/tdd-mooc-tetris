635b2ee6e67f018c1612ede6cde9d51f0c90c07c
Lauri Lavanti <laurilavanti@gmail.com>
2025-11-05 06:30:30 +0200

    feat: introduce Block

---
+++ src/Block.ts
@@ -0,0 +1,27 @@
+import { Shape, shapeToString } from "./Shape";
+
+export class Block implements Shape {
+  #character;
+
+  constructor(character: string) {
+    this.#character = character;
+  }
+
+  width() {
+    return 1;
+  }
+
+  height() {
+    return 1;
+  }
+
+  blockAt(row: number, col: number) {
+    if (row === 0 && col === 0) {
+      return this.#character;
+    }
+  }
+
+  toString() {
+    return shapeToString(this);
+  }
+}
