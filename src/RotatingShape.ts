2995ef627ae011bdb96c80390f8c13ca4bf6ab33
Lauri Lavanti <laurilavanti@gmail.com>
2025-11-06 10:06:14 +0200

    feat: add rotateRight

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -1,5 +1,13 @@
 import { Shape, shapeToString } from "./Shape";
 
+const createNewArrayArray = (size: number): string[][] => {
+    const array = new Array(size);
+    for (let row = 0; row < size; row++) {
+      array[row] = new Array(size);
+    }
+    return array;
+  }
+
 export class RotatingShape implements Shape {
     #shape: string[][];
 
@@ -7,12 +15,16 @@ export class RotatingShape implements Shape {
         return new RotatingShape(shape)
     }
 
-    constructor(shape: string) {
+    constructor(shape: string | string[][]) {
+        if (typeof shape === 'string') {
         this.#shape = shape
             .replaceAll(' ', '')
             .trim()
             .split('\n')
             .map((row) => row.split(''))
+        } else {
+            this.#shape = shape
+        }
     }
 
     width() {
@@ -31,4 +43,16 @@ export class RotatingShape implements Shape {
     toString() {
         return shapeToString(this)
     }
+
+    rotateRight() {
+        const size = this.#shape.length;
+        const rotated = createNewArrayArray(size);
+        for (let row = 0; row < size; row++) {
+          for (let column = 0; column < size; column++) {
+            rotated[row][column] = this.#shape[size - 1 - column][row];
+          }
+        }
+
+        return new RotatingShape(rotated);
+      }
 }
