5682d2474147b7d5b8b9d7541de205663a928e94
Lauri Lavanti <laurilavanti@gmail.com>
2025-11-06 12:29:17 +0200

    refactor: refactor Tetromino

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -3,31 +3,53 @@ import { Shape } from "./Shape";
 
 export class Tetromino implements Shape {
     static T_SHAPE = new Tetromino(
+        0,
+        4,
         `.T.
          TTT
          ...`
     )
 
-    #shape: RotatingShape
+    #currentOrientation: number
+    #orientations: RotatingShape[]
 
-    constructor(initialShape: string) {
-        const shape = new RotatingShape(initialShape)
-        this.#shape = shape
-    }  
+    constructor(
+        currentOrientation: number,
+        orientations: number,
+        initialShape?: string,
+    ) {
+        if (typeof initialShape === 'string') {
+            this.#currentOrientation = currentOrientation
+            const shape = new RotatingShape(initialShape)
+            this.#orientations = [
+                shape,
+                shape.rotateRight(),
+                shape.rotateRight().rotateRight(),
+                shape.rotateRight().rotateRight().rotateRight()
+            ].slice(0, orientations)
+        } else {
+            this.#currentOrientation = currentOrientation
+            this.#orientations = []
+        }
+    }
+
+    #shape() {
+        return this.#orientations[this.#currentOrientation]
+    }
     
     width() {
-        return this.#shape.width();
+        return this.#shape().width();
     }
     
     height() {
-        return this.#shape.height();
+        return this.#shape().height();
     }
     
     blockAt(row: number, col: number) {
-        return this.#shape.blockAt(row, col);
+        return this.#shape().blockAt(row, col);
     }
     
     toString() {
-        return this.#shape.toString();
+        return this.#shape().toString();
     }
 }
