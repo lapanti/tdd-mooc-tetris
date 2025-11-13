f7b4297d29c6f43faa9531b5559722ed17cf52fb
Lauri Lavanti <laurilavanti@gmail.com>
2025-11-13 11:08:13 +0200

    test: add test double for subscriber

---
+++ test/SubscribableBoard.test.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board";
+import { Subscriber } from "../src/Subscriber";
+
+class TestSubscriber implements Subscriber {
+    #lines: number
+
+    constructor() {
+        this.#lines = 0
+    }
+
+    notifyDeletedLines(lines: number): undefined {
+        this.#lines += lines
+    }
+
+    getLines(): number {
+        return this.#lines
+    }
+}
+
+describe("Subscribable Board", () => {
+  let board: Board;
+  beforeEach(() => {
+    board = new Board(3, 3);
+  });
+});
