b468885e352de7b52422fdc2fdf33c087fcde765
Lauri Lavanti <laurilavanti@gmail.com>
2025-11-13 14:03:40 +0200

    refactor: base scoring on level

--- src/ScoringSystem.ts
+++ src/ScoringSystem.ts
@@ -1,19 +1,21 @@
 export class ScoringSystem {
+    level: number
     score: number
 
     constructor() {
+        this.level = 0
         this.score = 0
     }
 
     linesCleared(lines: number): undefined {
         if (lines === 1) {
-            this.score += 40
+            this.score += 40 * (this.level + 1)
         } else if (lines === 2) {
-            this.score += 100
+            this.score += 100 * (this.level + 1)
         } else if (lines === 3) {
-            this.score += 300
+            this.score += 300 * (this.level + 1)
         } else {
-            this.score += 1200
+            this.score += 1200 * (this.level + 1)
         }
     }
 }
--- test/ScoringSystem.test.ts
+++ test/ScoringSystem.test.ts
@@ -17,7 +17,7 @@ describe("ScoringSystem", () => {
   })
 
   describe('scores cleared rows correctly', () => {
-    test('gets 40 points from clearing 1 line', () => {
+    test('gets 40 points from clearing 1 line (level 0)', () => {
         board = new Board(10, 10, [
             ['.','.','.','.','.','.','.','.','.','.'],
             ['.','.','.','.','.','.','.','.','.','.'],
@@ -46,7 +46,38 @@ describe("ScoringSystem", () => {
         expect(scores.score).to.eq(40)
     })
 
-    test('gets 80 points from clearing 2 lines in succession', () => {
+    test('gets 80 points from clearing 1 line (level 1)', () => {
+        board = new Board(10, 10, [
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['X','X','X','X','.','.','X','X','X','X'],
+          ])
+
+        const scores = new ScoringSystem()
+
+        scores.level = 1
+
+        board.onClearLine = (linesCleared: number) => {
+          scores.linesCleared(linesCleared)
+        }
+
+        board.drop(Tetromino.O_SHAPE)
+
+        for (let i = 0; i < 10; i++) {
+            board.tick()
+        }
+
+        expect(scores.score).to.eq(80)
+    })
+
+    test('gets 80 points from clearing 2 lines in succession (level 0)', () => {
         board = new Board(10, 10, [
             ['.','.','.','.','.','.','.','.','.','.'],
             ['.','.','.','.','.','.','.','.','.','.'],
@@ -81,7 +112,44 @@ describe("ScoringSystem", () => {
         expect(scores.score).to.eq(80)
     })
 
-    test('gets 100 points from clearing 2 lines at the same time', () => {
+    test('gets 240 points from clearing 2 lines in succession (level 2)', () => {
+        board = new Board(10, 10, [
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['X','X','X','X','.','.','X','X','X','X'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['X','X','X','X','.','.','X','X','X','X'],
+          ])
+
+        const scores = new ScoringSystem()
+
+        scores.level = 2
+
+        board.onClearLine = (linesCleared: number) => {
+          scores.linesCleared(linesCleared)
+        }
+
+        board.drop(Tetromino.O_SHAPE)
+
+        for (let i = 0; i < 10; i++) {
+            board.tick()
+        }
+
+        board.drop(Tetromino.O_SHAPE)
+
+        for (let i = 0; i < 10; i++) {
+            board.tick()
+        }
+
+        expect(scores.score).to.eq(240)
+    })
+
+    test('gets 100 points from clearing 2 lines at the same time (level 0)', () => {
         board = new Board(10, 10, [
             ['.','.','.','.','.','.','.','.','.','.'],
             ['.','.','.','.','.','.','.','.','.','.'],
@@ -110,7 +178,38 @@ describe("ScoringSystem", () => {
         expect(scores.score).to.eq(100)
     })
 
-    test('gets 300 points from clearing 3 lines at the same time', () => {
+    test('gets 400 points from clearing 2 lines at the same time (level 3)', () => {
+        board = new Board(10, 10, [
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['X','X','X','X','.','.','X','X','X','X'],
+            ['X','X','X','X','.','.','X','X','X','X'],
+          ])
+
+        const scores = new ScoringSystem()
+
+        scores.level = 3
+
+        board.onClearLine = (linesCleared: number) => {
+          scores.linesCleared(linesCleared)
+        }
+
+        board.drop(Tetromino.O_SHAPE)
+
+        for (let i = 0; i < 10; i++) {
+            board.tick()
+        }
+
+        expect(scores.score).to.eq(400)
+    })
+
+    test('gets 300 points from clearing 3 lines at the same time (level 0)', () => {
         board = new Board(10, 10, [
             ['.','.','.','.','.','.','.','.','.','.'],
             ['.','.','.','.','.','.','.','.','.','.'],
@@ -140,7 +239,39 @@ describe("ScoringSystem", () => {
         expect(scores.score).to.eq(300)
     })
 
-    test('gets 1200 points from clearing 4 lines at the same time', () => {
+    test('gets 1500 points from clearing 3 lines at the same time (level 4)', () => {
+        board = new Board(10, 10, [
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['X','X','X','X','X','.','X','X','X','X'],
+            ['X','X','X','X','X','.','X','X','X','X'],
+            ['X','X','X','X','X','.','X','X','X','X'],
+          ])
+
+        const scores = new ScoringSystem()
+        
+        scores.level = 4
+
+        board.onClearLine = (linesCleared: number) => {
+          scores.linesCleared(linesCleared)
+        }
+
+        board.drop(Tetromino.I_SHAPE)
+        board.rotateRight()
+
+        for (let i = 0; i < 10; i++) {
+            board.tick()
+        }
+
+        expect(scores.score).to.eq(1500)
+    })
+
+    test('gets 1200 points from clearing 4 lines at the same time (level 0)', () => {
         board = new Board(10, 10, [
             ['.','.','.','.','.','.','.','.','.','.'],
             ['.','.','.','.','.','.','.','.','.','.'],
@@ -168,6 +299,38 @@ describe("ScoringSystem", () => {
         }
 
         expect(scores.score).to.eq(1200)
+    })
+
+    test('gets 7200 points from clearing 4 lines at the same time (level 5)', () => {
+        board = new Board(10, 10, [
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.'],
+            ['X','X','X','X','X','.','X','X','X','X'],
+            ['X','X','X','X','X','.','X','X','X','X'],
+            ['X','X','X','X','X','.','X','X','X','X'],
+            ['X','X','X','X','X','.','X','X','X','X'],
+          ])
+
+        const scores = new ScoringSystem()
+
+        scores.level = 5
+
+        board.onClearLine = (linesCleared: number) => {
+          scores.linesCleared(linesCleared)
+        }
+
+        board.drop(Tetromino.I_SHAPE)
+        board.rotateRight()
+
+        for (let i = 0; i < 10; i++) {
+            board.tick()
+        }
+
+        expect(scores.score).to.eq(7200)
     })
   })
 });
